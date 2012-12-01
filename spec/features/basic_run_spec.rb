825290dd908a72400e45e1c453a4500977bbe9c9
Jason Draper <jason@drapergeek.com>
2012-12-01 13:23:30 -0500

    Set up basic smoke test.

--- spec/features/basic_run_spec.rb
+++ spec/features/basic_run_spec.rb
@@ -1,6 +1,15 @@
 describe 'it can exceute the main file' do
   it "should return normal exit status" do
-    command = "ruby ../../get_rtp_stats"
-    system(command).should be_false
+    return_text = `ruby get_rtp_stats -d 192.168.5.11 -g 192.168.5.11 -p 2003 -C public -v `
+
+    return_text.should include 'Verbose option enabled'
+    return_text.should include '192.168.5.11 down, no SNMP manager created'
+    return_text.should include 'Not sending data to 192.168.5.11 while in verbose mode'
+  end
+
+  it "should have the correct ip" do
+    return_text = `ruby get_rtp_stats -d 192.168.5.11 -g 192.168.5.11 -p 2003 -C public -v `
+
+    return_text.should include '192.168.5.11'
   end
 end
