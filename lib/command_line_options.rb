7a5954eebe60e9a9bb3a1ff572e352dba048eb62
Jason Draper <jason@drapergeek.com>
2012-12-01 14:16:49 -0500

    Move CommandLineOptions and Printer class out

---
+++ lib/command_line_options.rb
@@ -0,0 +1,50 @@
+class CommandLineOptions
+  def self.parse(args)
+    script_version = "0.01"
+    options = OpenStruct.new
+    options.device_list = []
+    options.community = "public" # default to a common community string
+    options.warning = "0"
+    options.critical = "0"
+    options.exit_status = "3"
+    options.maximum = "0"
+    options.verbose = false
+
+    opts = OptionParser.new do |user_input|
+      user_input.banner = "Usage: #{user_input.program_name} [options]"
+      user_input.on("-d x y z", "--device x y z", Array, "Input list of devices seperated by a comma") do |device|
+        options.device_list = device
+      end
+      user_input.on("-g String", "--graphite String", String, "Input hostname of the graphite server") do |graphite|
+        options.graphite_server = graphite
+      end
+      user_input.on("-p Integer", "--port Integer", Integer, "Input port number for graphite instance") do |port|
+        options.graphite_server_port = port
+      end
+      user_input.on("-C String", "--community String", String, "Specify snmp community") do |community|
+        options.community = community
+      end
+      user_input.on("-v", "--verbose", "Run verbosely (debugging only)") do |verbose|
+        $VERBOSE = true
+      end
+      user_input.on("-V", "--version", "Prints Version of this script.") do
+        puts "get_rtp_stats #{script_version}"
+        exit
+      end
+      user_input.on_tail("-h", "--help", "Show help message") do
+        puts opts
+        exit
+      end
+    end
+
+    begin
+      opts.parse!(args)
+    rescue OptionParser::InvalidOption
+      puts opts
+      exit
+    end
+    options
+
+  end
+
+end
---
+++ lib/printer.rb
@@ -0,0 +1,21 @@
+class Printer
+  attr_reader :commands
+
+  def initialize
+    @commands = []
+  end
+
+  def add_command(command)
+    commands << command
+  end
+
+  def print_to_screen
+    commands.each do |command|
+      puts command
+    end
+  end
+
+  def request
+    commands.join("\n") + "\n"
+  end
+end
